58a6baea98b0cfe2117494831bac9c86b0836edb
Chris Copeland <chris@chrisnc.net>
2019-03-30 20:35:28 -0700

    build gcc

--- build.bash
+++ build.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 # Adapted from https://wiki.osdev.org/GCC_Cross-Compiler
 
 topdir="$(pwd)"
@@ -18,5 +20,20 @@ pushd $binutils_builddir
 "$topdir/binutils/configure" --target="$target" --prefix="$prefix" --with-sysroot --disable-nls --disable-werror
 make --jobs $(nproc)
 make install
+
+popd
+
+which -- "$target-as" || echo "$target-as" is not in the PATH
+
+
+# gcc
+
+gcc_builddir="build/$target/gcc"
+mkdir -p $gcc_builddir
+pushd $gcc_builddir
+
+"$topdir/gcc/configure" --target="$target" --prefix="$prefix" --with-sysroot --with-newlib --disable-nls --enable-languages=c,c++
+make --jobs $(nproc) all-gcc all-target-libgcc
+make install-gcc install-target-libgcc
 
 popd
